0925e8863084b0e69f638d46c432922178c38b97
mariamnatroshvili3000 <112975366+mariamnatroshvili3000@users.noreply.github.com>
2022-10-24 21:05:10 +0400

    sf
    ssfef

---
+++ script.js
@@ -0,0 +1,44 @@
+var strSum = prompt ("შეიყვანეთ ნივთის ფასი", 80)
+if (price >= 20 && income <=50) {
+    alert( "sashualo fasi")
+}
+
+var price = 20
+if (price < 20){
+    alert("იაფი")
+}
+else {
+    alert ("ძვირი")
+
+    var price = 200;
+    switch (price) {
+        case 10:
+            console.log ("იაფი")
+            break;
+        case 20 :
+            console.log ("საშუალო")
+            break
+        case 80 :
+            console.log ("კაი ძვირი")
+    }    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
